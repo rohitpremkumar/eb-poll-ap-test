d80d1067451d1e09e00fe31529a9f51287c26d3e
Rohit Premkumar <kumar.rohitp098@gmail.com>
2018-08-04 00:14:42 -0400

    prod settings

---
+++ .env.py
@@ -0,0 +1,6 @@
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = 'iloverashi'
+EMAIL_HOST_USER = 'kumar.rohitp098@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+SECRET_KEY = 't*jdk3eu(%+mmc=32%i&6)vatwy+&_09xa5%8d#e0(m1uimjsb'
